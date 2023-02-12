45833735d5c52b78a0fc388f3ef5e1dee0dcc5db
Shivam Kumar Jha <shivamjhaaaa@gmail.com>
2023-02-12 10:17:17 +0530

    third commit

--- src/com/git.java
+++ src/com/git.java
@@ -3,10 +3,17 @@ package com;
 public class git {
 }
 /**
+ * TO MAKE A REPOSITORY AND PUSH CODE
  * git init -b main //to initialize the main branch from git
  * git add .
  * git commit -m "initial commit"
  * git remote add origin <repository ssh link>
  * git remote -v // to verify
  * git push -u origin main // to push all your code to github repo
+ *
+ * TO UPDATE THE REPOSITORY
+ * git add .
+ * git commit -m "second commit"
+ * git push -u origin main
+ *
  */
