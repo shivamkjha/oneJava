1d66c54c902099599cf72a4879f3f7847846c1b0
Shivam Kumar Jha <shivamjhaaaa@gmail.com>
2023-02-12 10:15:24 +0530

    second commit

---
+++ src/com/git.java
@@ -0,0 +1,12 @@
+package com;
+
+public class git {
+}
+/**
+ * git init -b main //to initialize the main branch from git
+ * git add .
+ * git commit -m "initial commit"
+ * git remote add origin <repository ssh link>
+ * git remote -v // to verify
+ * git push -u origin main // to push all your code to github repo
+ */
